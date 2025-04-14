9d50fa6822c4b071d0be807e2d61aceca098317b
Priyanshu Walia <waliapriyanshu07@gmail.com>
2025-04-14 22:12:17 +0530

    added components

---
+++ frontend/src/components/HourglassLoader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HourglassLoader = () => {
+  return (
+    <div className="loader-overlay">
+      <div className="hourglass"></div>
+    </div>
+  );
+};
+
+export default HourglassLoader;
